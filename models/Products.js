f5d11f79e46f07c8ae60b7aab9b7b0723861616f
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-01-04 20:17:18 +0600

    set up all and connect to Database

---
+++ models/Products.js
@@ -0,0 +1,41 @@
+const { ObjectID } = require("bson");
+const mongoose = require("mongoose");
+
+//schema design
+const productSchema = mongoose.Schema({
+    imageUrl: [{
+        type: String,
+        required: true,
+        validate: [validator.isURL , "Plese Provide valid URL"]
+    }],
+    name: {
+      type: String,
+      required: [true, "Please provide a name for thid product"],
+      trim: true,
+      lowercase:true,
+      unique: [true, "Name must be Unique"],
+      minLength: [3, "Name mast be 3 characters"],
+      maxLength: [100, "Name is too larges"],
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    category:{
+      type:String,
+      require:true,
+    },
+    price: {
+        type: Number,
+        require: true,
+        min: [0, "product price can not Nagative"]
+    },
+    
+  }, {
+    timestamps: true
+  })
+  
+  //model
+  const Products = mongoose.model('Products', productSchema)
+
+  module.exports=Products;
